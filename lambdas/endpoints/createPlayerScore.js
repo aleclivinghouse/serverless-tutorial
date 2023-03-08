02fb0af223886885d587f12a088a196734c57ec9
Your Name <you@example.com>
2023-03-07 16:22:13 -0800

    hooks done

--- lambdas/endpoints/createPlayerScore.js
+++ lambdas/endpoints/createPlayerScore.js
@@ -1,25 +1,31 @@
 const Responses = require('../common/API_Responses');
 const Dynamo = require('../common/Dynamo');
+const { withHooks, hooksWithValidation } = require('../common/hooks');
+const yup = require('yup');
+
 const tableName = process.env.tableName;
 
-exports.handler = async event => {
-    console.log('event', event);
-    if(!event.pathParameters || !event.pathParameters.ID){
-        return Responses._400({message: 'Missing the ID from the path'})
-    }
+const bodySchema = yup.object().shape({
+    name: yup.string().required(),
+    score: yup.number().required(),
+});
 
+const pathSchema = yup.object().shape({
+    ID: yup.string().required(),
+});
+
+const handler = async event => {
     let ID = event.pathParameters.ID;
-    const user = JSON.parse(event.body);
-    user.ID = ID
+    const user = event.body;
+    user.ID = ID;
 
-    const newUser = await Dynamo.write(user, tableName).catch(err => {
-        console.log("this is the error in createPlayerScore: ", err);
-        return null;
-    })
+    const newUser = await Dynamo.write(user, tableName);
 
-    if(!newUser){
-        return Responses._400({message: 'Failed to get user by ID'})
+    if (!newUser) {
+        return Responses._400({ message: 'Failed to write user by ID' });
     }
 
-    return Responses._200({newUser});
-} 
+    return Responses._200({ newUser });
+};
+
+exports.handler = hooksWithValidation({ bodySchema, pathSchema })(handler);
--- lambdas/endpoints/getPlayerScore.js
+++ lambdas/endpoints/getPlayerScore.js
@@ -1,21 +1,22 @@
 const Responses = require('../common/API_Responses');
 const Dynamo = require('../common/Dynamo');
 const tableName = process.env.tableName;
+const {withHooks} = require('../common/hooks');
 
-exports.handler = async event => {
-    console.log('event', event);
-    if(!event.pathParameters || !event.pathParameters.ID){
+const handler = async event => {
+
+    if(!event.pathParameters.ID){
         return Responses._400({message: 'Missing the ID from the path'})
     }
 
     let ID = event.pathParameters.ID;
-    const user = await Dynamo.get(ID, tableName).catch(err =>{
-        console.log("error in Dynamo GET: ", err);
-        return null
-    });
+    const user = await Dynamo.get(ID, tableName);
+    
     if(!user){
         return Responses._400({message: 'Failed to get user by ID'})
     }
 
     return Responses._200({user});
 } 
+
+exports.handler = withHooks(handler);
